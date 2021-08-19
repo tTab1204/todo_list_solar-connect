dc007f1d9b5ef25963bd99a37bd9413a1d7c0888
JuYoung <ken44929@gmail.com>
2021-08-19 20:55:42 +0900

    Feat: #1 Add current time

--- src/components/todo/template/head/TodoHead.tsx
+++ src/components/todo/template/head/TodoHead.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useRef, useState } from 'react';
+import moment from 'moment';
+import styled from 'styled-components';
 
 const TodoHeadBlock = styled.div`
   display: flex;
@@ -9,27 +10,47 @@ const TodoHeadBlock = styled.div`
   border-bottom: 3px solid #33bb77;
 `;
 
-const DateText = styled.div`
-  font-size: 26px;
-  color: #119955;
-  padding-left: 10px;
-`;
-
 const DayText = styled.div`
   font-size: 22px;
   color: #119955;
   padding-top: 5px;
 `;
 
+const DateText = styled.div`
+  font-size: 26px;
+  color: #119955;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const TimeText = styled.div`
+  font-size: 26px;
+  color: #119955;
+  padding-left: 10px;
+`;
+
 const TodoHead = () => {
-  //@TODO 현재 시간을 표시해야합니다.
-  const dayString = "Tuesday";
-  const dateString = "July 20, 2021";
+  let timer: any = null;
+  const [time, setTime] = useState(moment());
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      setTime(moment());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const dayString = time.format('LLLL').slice(0, 9);
+  const dateString = time.format('LL');
+  const timeString = time.format('LTS');
 
   return (
     <TodoHeadBlock>
       <DayText>{dayString}</DayText>
       <DateText>{dateString}</DateText>
+      <TimeText>{timeString}</TimeText>
     </TodoHeadBlock>
   );
 };
